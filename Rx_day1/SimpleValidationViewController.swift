6b7f8edc3745f13a11245a23a22721a74e7eb6e6
heze <895315401@qq.com>
2018-07-31 18:48:25 +0800

    练习

---
+++ Rx_day1/SimpleValidationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SimpleValidationViewController.swift
+//  Rx_day1
+//
+//  Created by 何泽的mac on 2018/7/31.
+//  Copyright © 2018年 何泽的mac. All rights reserved.
+//
+
+import UIKit
+
+class SimpleValidationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
